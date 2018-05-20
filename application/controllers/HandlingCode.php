a8c0aa4ba3a2a357c17e82809e856d728339626c
Shoukhin <noushad@webpers.com>
2018-05-20 15:30:51 +0600

    Handling code completed except review

--- application/controllers/HandlingCode.php
+++ application/controllers/HandlingCode.php
@@ -6,46 +6,57 @@ class HandlingCode extends CI_Controller {
 	public function __construct()
     {
 		parent::__construct();
-		if (!$this->ion_auth->logged_in())
-    	{
+		
+		if (!$this->ion_auth->logged_in()){
     		$this->session->set_flashdata('message', "Please login first!!");
     		redirect('login', 'refresh');
     	}
+    	
     	$_SESSION['record_id'] = 2;//This line will have to customize after completing the project
+		
 		$this->load->helper('CID/nav');
 		$this->load->library('form_validation');
-		$this->load->model('Handling_code_model');		
+		$this->load->model('Handling_code_model');
+
+		$text_exists = $this->Handling_code_model->remaining_text($_SESSION['record_id']);
+    	if($text_exists == 0){
+    		redirect('text','refresh');
+    	}
 	}
 
 	public function index(){
 		$record_id  = $_SESSION['record_id'];
+		$txt_id     = $this->Handling_code_model->next($record_id);
 
-		if($this->session->has_userdata('txt_id')){
-			$txt_id = $_SESSION['txt_id'];
-		}
-
-		if(isset($txt_id)){
-			$data['text'] = $this->Handling_code_model->get_text_info($record_id, $txt_id);
+		if(!is_null($txt_id)){
+			$data['text']  = $this->Handling_code_model->get_text_info($record_id, $txt_id);
+			$remainingText = $this->Handling_code_model->remaining_text($record_id, $txt_id);
+			if($remainingText == 0){
+				redirect('review/','refresh');
+			}
+			$data['remainingTextVeiw'] = $remainingText - 1;
 		}else{
-			$data['text'] = $this->Handling_code_model->get_text_info($record_id);
+			$data['text']          = $this->Handling_code_model->get_text_info($record_id);
+			$data['remainingTextVeiw'] = $this->Handling_code_model->remaining_text($record_id)-1;
 		}
+
 		$this->load->view('dashboard/handlingcode', $data);
 	}
 
 	public function create(){
 		$config = array(
 		        array(
-		                'field' => 'handlingcode',
-		                'label' => 'Handling Code',
-		                'rules' => 'required',
+		                'field'  => 'handlingcode',
+		                'label'  => 'Handling Code',
+		                'rules'  => 'required',
 		                'errors' => array(
 		                        'required' => 'Please select a handling code.',
 		                ),
 		        ),
 		        array(
-		                'field' => 'textID',
-		                'label' => 'Text ID',
-		                'rules' => 'required',
+		                'field'  => 'textID',
+		                'label'  => 'Text ID',
+		                'rules'  => 'required',
 		                'errors' => array(
 		                        'required' => 'Sorry, you can\'t submit the form without text ID.',
 		                ),
@@ -53,7 +64,7 @@ class HandlingCode extends CI_Controller {
 		        array(
 		                'field' => 'instruction',
 		                'label' => 'Instruction',
-		                'rules' => 'required'
+		                'rules' => 'required|trim'
 		        )
 		);
 
@@ -66,17 +77,20 @@ class HandlingCode extends CI_Controller {
 			$this->session->set_flashdata('textID', form_error('textID'));
 			$this->session->set_flashdata('instruction', form_error('instruction'));
 
-			$_SESSION['handlingcodeInput'] = $this->input->post('handlingcode');
-        	$_SESSION['instructionInput']  = $this->input->post('instruction');
+			$this->session->set_flashdata('handlingcodeInput', $this->input->post('handlingcode'));
+        	$this->session->set_flashdata('instructionInput', $this->input->post('instruction'));
 
             redirect('handlingcode/','refresh');
 
         }else{
+        	$data['record_id']   = $_SESSION['record_id'];
+        	$data['text_id']     = $this->input->post('textID');
+        	$data['code']        = $this->input->post('handlingcode');
+        	$data['instruction'] = $this->input->post('instruction');
 
-        	$handlingcode = $this->input->post('handlingcode');
-        	$txt_id       = $this->input->post('textID');
-        	$instruction  = $this->input->post('instruction');
+        	$this->Handling_code_model->record_handling_code($data);
 
+        	redirect('handlingcode/','refresh');
         }
 	}
 
--- application/views/dashboard/review-main.php
+++ application/views/dashboard/review-main.php
@@ -186,7 +186,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> 
         <!-- <script type="text/javascript" src="../../js/handlingcode.js"></script> -->
         <script type="text/javascript" src="<?php echo base_url(); ?>js/review.js"></script>
-        <!-- <script type="text/javascript" src="<?php echo base_url(); ?>js/protectivemarking.js"></script> -->
+        <script type="text/javascript" src="<?php echo base_url(); ?>js/protectivemarking.js"></script>
         <script type="text/javascript">
             addEventListener('load',function(e){
                 $.ajax({
