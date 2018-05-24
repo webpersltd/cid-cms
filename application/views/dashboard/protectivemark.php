de7b6beed4ae8f58a9228c2c21abd971d296e3f2
Shoukhin <noushad@webpers.com>
2018-05-24 15:28:23 +0600

    Handling review worked except cross button strategy

--- application/views/dashboard/protectivemark.php
+++ application/views/dashboard/protectivemark.php
@@ -10,64 +10,64 @@
     
     <body>
         <nav class="navbar navbar-default">
-                    <div class="container-fluid">
-                      <!-- Brand and toggle get grouped for better mobile display -->
-                      <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                          <span class="sr-only">Toggle navigation</span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Entity manager</a>
-                      </div>
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Entity manager</a>
+                </div>
                   
-                      <!-- Collect the nav links, forms, and other content for toggling -->
-                      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav">
-                          <li><a href=""></a></li>
-                          <li><a href=""></a></li>
-                          <li><a href=""></a></li>
-                          <li><a href=""></a></li>
-                          <li><a href=""></a></li>
-                          <li><a href=""></a></li>
-                          <?php cid_cms_tab(); ?>
-                          <li><a href=""></a></li>
-                          <li><a href=""></a></li>
-                          <li><a href="">Database : Custom Intelligence Database(CID)</a></li>
-                        </ul>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <?php cid_cms_tab(); ?>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href="">Database : Custom Intelligence Database(CID)</a></li>
+                    </ul>
                         
-                        <ul class="nav navbar-nav navbar-right">
-                          
-                          <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user" aria-hidden="true">&nbsp</span>Hi , Jon Doe <span class="caret"></span></a>
+                    <ul class="nav navbar-nav navbar-right">                          
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <span class="glyphicon glyphicon-user" aria-hidden="true">&nbsp</span>Hi , Jon Doe <span class="caret"></span>
+                            </a>
                             <ul class="dropdown-menu">
-                              <li><a href="#"><span class="glyphicon glyphicon-pencil" aria-hidden="true">&nbsp</span>Change password</a></li>
-                              
+                              <li><a href="#"><span class="glyphicon glyphicon-pencil" aria-hidden="true">&nbsp</span>Change password</a></li>                              
                               <li role="separator" class="divider"></li>
                               <li><a href="#"><span class="glyphicon glyphicon-off" aria-hidden="true">&nbsp</span>Log out</a></li>
                             </ul>
-                          </li>
-                        </ul>
-                      </div><!-- /.navbar-collapse -->
-                    </div><!-- /.container-fluid -->
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
         </nav>
         <div class="container-fluid">
             <div class="col-md-2">
-            <ul class="list-group">
-                <li  class="list-group-item"><a href="<?php echo base_url(); ?>initials/"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span>&nbsp&nbspInitials</a></li>
-                <li class="list-group-item"><a href="<?php echo base_url(); ?>subjects/"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span>&nbsp&nbspSubjects</a></li>
-                <li class="list-group-item"><a href="<?php echo base_url(); ?>text/"><span class="glyphicon glyphicon-text-size" aria-hidden="true"></span>&nbsp&nbspText</a></li>
-                <li class="list-group-item"><a href="<?php echo base_url(); ?>handlingcode/"><span class="glyphicon glyphicon-compressed" aria-hidden="true"></span>&nbsp&nbspHandling code</a></li>
-                <li class="list-group-item"><a href="<?php echo base_url(); ?>protectivemark/"><span class="glyphicon glyphicon-registration-mark" aria-hidden="true"></span>&nbsp&nbspProtective</a></li>
-                <!-- <li class="list-group-item"><a href="#"><span class="glyphicon glyphicon-copyright-mark" aria-hidden="true"></span>&nbsp&nbspMarking</a></li>-->
-                <li class="list-group-item"><a href="<?php echo base_url(); ?>review/"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>&nbsp&nbspReview</a></li>
-                <li class="list-group-item"><a href="<?php echo base_url(); ?>dissemination/"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span>&nbsp&nbspDissemination</a></li>
-                <li class="list-group-item"><a href="<?php echo base_url(); ?>search/"><span class="glyphicon glyphicon-search" aria-hidden="true"></span>&nbsp&nbspSearch</a></li>
-                <li class="list-group-item"><a href="<?php echo base_url(); ?>viewlog/"><span class="glyphicon glyphicon-book" aria-hidden="true"></span>&nbsp&nbspView log</a></li>
-            </ul>
+                <ul class="list-group">
+                    <li  class="list-group-item"><a href="<?php echo base_url(); ?>initials/"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span>&nbsp&nbspInitials</a></li>
+                    <li class="list-group-item"><a href="<?php echo base_url(); ?>subjects/"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span>&nbsp&nbspSubjects</a></li>
+                    <li class="list-group-item"><a href="<?php echo base_url(); ?>text/"><span class="glyphicon glyphicon-text-size" aria-hidden="true"></span>&nbsp&nbspText</a></li>
+                    <li class="list-group-item"><a href="<?php echo base_url(); ?>handlingcode/"><span class="glyphicon glyphicon-compressed" aria-hidden="true"></span>&nbsp&nbspHandling code</a></li>
+                    <li class="list-group-item"><a href="<?php echo base_url(); ?>protectivemark/"><span class="glyphicon glyphicon-registration-mark" aria-hidden="true"></span>&nbsp&nbspProtective</a></li>
+                    <!-- <li class="list-group-item"><a href="#"><span class="glyphicon glyphicon-copyright-mark" aria-hidden="true"></span>&nbsp&nbspMarking</a></li>-->
+                    <li class="list-group-item"><a href="<?php echo base_url(); ?>review/"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>&nbsp&nbspReview</a></li>
+                    <li class="list-group-item"><a href="<?php echo base_url(); ?>dissemination/"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span>&nbsp&nbspDissemination</a></li>
+                    <li class="list-group-item"><a href="<?php echo base_url(); ?>search/"><span class="glyphicon glyphicon-search" aria-hidden="true"></span>&nbsp&nbspSearch</a></li>
+                    <li class="list-group-item"><a href="<?php echo base_url(); ?>viewlog/"><span class="glyphicon glyphicon-book" aria-hidden="true"></span>&nbsp&nbspView log</a></li>
+                </ul>
             </div>
-            <div class="col-md-8">
+            <div class="col-md-10">
                 <nav class="breadcrumb">
                     <a class="breadcrumb-item" href="#">INITIALS&nbsp;&nbsp;<span class="glyphicon glyphicon-menu-right"></span></a>
                     <a class="breadcrumb-item" href="#">SUBJECT&nbsp;&nbsp;<span class="glyphicon glyphicon-menu-right"></span></a>
@@ -80,88 +80,94 @@
              </div>
              <div  class="col-md-10">                    
                 <div class="well">
-                    <b>Completion Note:</b> You must review each line of text and apply the correct Handling Code.
+                    <b>Completion Note:</b> You must review each line of text and apply a Protective Marking to the overall record. This Protective Marking will apply to all of the text and will directly impact whether information can be Disseminated. You must ensure you take appropriate care when apply the correct Protective Marking.
                 </div>
                 <div class="col-md-9 handling-heading">
-                    <h4>SUMMARY OF TEXT AND EVALUATIONS:</h4>
+                    <h4>SUMMARY OF RECORD:</h4>
                 </div>
-                <div class="col-md-1 grade-title">
-                   
+                <div class="col-md-1 grade-title">                   
                 </div>
+                <div id="container_handling_code">                    
+                </div>
+
+                <?= form_open('HandlingCode/create'); ?>
                 <div id="container_handling_code">
-                    <!-- <div id="text_area" class="col-md-8">
-                        
-                         <p>Auto Completed</p> 
+                    <div id="text_area" class="col-md-10">
+                        <textarea id="" name="summary" rows="5" style="width:100%" disabled>Summary of Record</textarea>
                     </div>
-                    <div class="col-md-2">
+                    <div id="grading" class="col-md-2">
+                        <h4>GRADING</h4>
                         <table  class="table grade-table table-bordered">
                             <tbody id="grading-body">
-                               
-                           </tbody>
-                        </table>
-                    </div>
-                    <div class="col-md-10 handling-grade">
-                        <table class="table handling-table table-bordered">
-                            <tbody>
-                            <tr>
-                                <td>
-                                    <b>HANDLING CODE</b><br>
-                                    To be completed by <br>
-                                    the evaluator on <br>
-                                    receipt and prior to <br>
-                                    entry ont the <br>
-                                    intelligence system.
-                                </td>
-                                <td class="functional">
-                                    <b>1</b><br>
-                                    Permits dissemination<br>
-                                    within Customs and to <br>
-                                    other law inforcement <br>
-                                    agencies in Bangladesh <br>
-                                    as specified.
-                                </td>
-                                <td class="functional">
-                                    <b>2</b><br>
-                                    Permits<br>
-                                    dissemination to <br>
-                                    Bangladesh non <br>
-                                    prosecuting parties
-                                </td>
-                                <td class="functional">
-                                    <b>3</b><br>
-                                    Permits<br>
-                                    dissemination to <br>
-                                    foreign law <br>
-                                    agencies
-                                </td>
-                                <td class="functional">
-                                    <b>4</b><br>
-                                    Permits dissemination<br>
-                                    within Bangladesh<br>
-                                    Customs only: specify<br>
-                                    reasons and internal<br>
-                                    recipient(s)
-                                </td>
-                                <td class="functional">
-                                    <b>5</b><br>
-                                    Permits<br>
-                                    dissemination, but<br>
-                                    receiving agency to<br>
-                                    observe conditions<br>
-                                    as specified
-                                </td>
-                            </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>2</td>
+                                    <td id="handlingcode">3</td>
+                                    <input type="hidden" id="handlingcodeInput" name="handlingcode" value="">
+                                    <input type="hidden" id="text_id" name="textID" value="">
+                                </tr>
                             </tbody>
                         </table>
                     </div>
-                    
-                    <div class="col-md-10 handling-heading">
-                        <h4>DETAILED HANDLING INSTRUCTIONS:</h4>
+                    <div class="col-md-10 handling-grade">
+                        <?php
+                        /*if( !empty($this->session->flashdata('handlingcode')) ){
+                            echo $this->session->flashdata('handlingcode');
+                        }*/
+                        ?>
+                        <table class="table handling-table table-bordered">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <b>HANDLING CODE</b>
+                                        <br>To be completed by
+                                        <br>the evaluator on
+                                        <br>receipt and prior to
+                                        <br>entry ont the
+                                        <br>intelligence system.
+                                    </td>
+                                    <td class="functional">
+                                        <b>1</b>
+                                        <br>Permits dissemination
+                                        <br>within Customs and to
+                                        <br>other law inforcement 
+                                        <br>agencies in Bangladesh 
+                                        <br>as specified.
+                                    </td>
+                                    <td class="functional">
+                                        <b>2</b>
+                                        <br>Permits
+                                        <br>dissemination to
+                                        <br>Bangladesh non
+                                        <br>prosecuting parties
+                                    </td>
+                                    <td class="functional">
+                                        <b>3</b>
+                                        <br>Permits
+                                        <br>dissemination to
+                                        <br>foreign law
+                                        <br>agencies
+                                    </td>
+                                    <td class="functional">
+                                        <b>4</b>
+                                        <br>Permits dissemination
+                                        <br>within Bangladesh
+                                        <br>Customs only: specify
+                                        <br>reasons and internal
+                                        <br>recipient(s)
+                                    </td>
+                                    <td class="functional">
+                                        <b>5</b>
+                                        <br>Permits
+                                        <br>dissemination, but
+                                        <br>receiving agency to
+                                        <br>observe conditions
+                                        <br>as specified
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
-                    <div class="col-md-10 handling-heading">
-                        <textarea disabled class="form-control" rows="5" id="instructions"></textarea>
-                    </div> -->
-                </div>
                 
                 <div class="container-fluid">
                    
--- js/handlingcodereview.js
+++ js/handlingcodereview.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     $("#review_ok").click(function(){
-        var id  = $("input[name=handlingcodeID]").val();
-        var url = "http://localhost/CID/reviewdone/";
+        var id                = $("input[name=handlingcodeID]").val();
+        var url               = "http://localhost/CID/reviewdone/";
+        var remaining         = $('#remaining').text().charAt(23);
+        var remainingInt      = parseInt(remaining);
+        remainingInt++;
+        var fullRemainingText = $('#remaining').text();
+        fullRemainingText     = setCharAt(fullRemainingText,23,remainingInt);
         $.post(
             url, 
             {handlingCodeID: id}, 
@@ -15,10 +20,16 @@ $(document).ready(function(){
                     $("#code").text(result.codeInfo);
                     $("#instruction").text(result.instruction);
                     $("input[name=handlingcodeID]").val(result.handlingCodeid);
+                    $("#remaining").text(fullRemainingText);
                 }
             }, "json"
         );
     });
+
+    function setCharAt(str,index,chr) {
+        if(index > str.length-1) return str;
+        return str.substr(0,index) + chr + str.substr(index+1);
+    }
 
     $(document).on({
         ajaxStart: function() { $(".loader").show(); },
