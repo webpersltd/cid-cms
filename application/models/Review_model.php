539fcdfcd5c691f36d770f87a60afc7d1da870f1
Shoukhin <noushad@webpers.com>
2018-06-07 10:25:56 +0600

    Final Review Done

--- application/models/Review_model.php
+++ application/models/Review_model.php
@@ -4,6 +4,7 @@ class Review_model extends CI_Model {
     public function get_details($record_id)
     {
         $text_id  = $this->check_existing_data($record_id);
+        $has_data = $this->has_data($record_id);
         
   		$this->db->select('*, texts.id as txtID');
         $this->db->from('texts');
@@ -19,6 +20,12 @@ class Review_model extends CI_Model {
         $this->db->limit(1);
 
         $query = $this->db->get();
+
+        if(!is_null($has_data)){
+            $query->row()->details_reviewed = $has_data->details_reviewed;
+            $query->row()->pm_reviewed      = $has_data->pm_reviewed;
+        }
+
         return $query->row();
     }
 
@@ -71,4 +78,33 @@ class Review_model extends CI_Model {
         return $query->row()->text_id;
     }
 
+    public function count_final_review_data($record_id){
+        $this->db->select('text_id');
+        $this->db->join('texts', 'texts.id = final_review.text_id');
+        $this->db->where('texts.record_id', $record_id);
+        $this->db->where('details_reviewed', 1);
+        $this->db->where('pm_reviewed', 1);
+
+        $query = $this->db->get('final_review');
+        return $query->num_rows();
+    }
+
+    public function has_data($record_id){
+        $where = "details_reviewed!='1' OR pm_reviewed!='1'";
+
+        $this->db->select('*');
+        $this->db->join('texts', 'texts.id = final_review.text_id');
+        $this->db->where('texts.record_id', $record_id);
+        $this->db->where($where);
+
+        $query = $this->db->get('final_review');
+        return $query->row();
+    }
+
+    public function total_text($record_id){
+        $this->db->where('record_id', $record_id);
+        $query = $this->db->get('texts');
+
+        return $query->num_rows();
+    }
 }
